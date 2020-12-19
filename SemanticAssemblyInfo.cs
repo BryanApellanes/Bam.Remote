b2446dfb80d480c36c407ac4e7a451dd5fe03f73
Bryan Apellanes <bryan.apellanes@gmail.com>
2020-12-19 06:26:16 -0800

    $1

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.2786528605")]
-[assembly: AssemblyCommit("8d6b2d4")]
-[assembly: AssemblySemanticVersion("1.0.5-test+8d6b2d4")]
+[assembly: AssemblyFileVersion("1.0.5.2018371695")]
+[assembly: AssemblyCommit("e44f550")]
+[assembly: AssemblySemanticVersion("1.0.5-test+e44f550")]
