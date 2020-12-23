88e5debae6fb49c5f85248622abf65ffad09cbb8
Bryan Apellanes <bryan.apellanes@gmail.com>
2020-12-23 14:26:44 -0800

    pre submodule commit: fix

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -2,14 +2,8 @@ using System.Reflection;
 using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
-<<<<<<< HEAD
-[assembly: AssemblyFileVersion("1.0.5.2018371695")]
-[assembly: AssemblyCommit("e44f550")]
-[assembly: AssemblySemanticVersion("1.0.5-test+e44f550")]
-=======
 [assembly: AssemblyFileVersion("1.0.5.0")]
 [assembly: AssemblyCommit("e44f550")]
 [assembly: AssemblySemanticVersion("1.0.5-test+e44f550")]
 [assembly: AssemblyDescription("; SemanticVersion=1.0.5-test+e44f550, Revision=1.0.5.2018371695")]
 [assembly: AssemblyProduct("bam.remote")]
->>>>>>> d1ffa46... updated build scripts for app services
