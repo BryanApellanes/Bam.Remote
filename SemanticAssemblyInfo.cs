610f5f3fd57ecdfab22131a875c46c680230e10f
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-06-11 20:34:13 -0700

    pre submodule commit: update

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -3,7 +3,7 @@ using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.6.0")]
 [assembly: AssemblyFileVersion("1.0.6.0")]
-[assembly: AssemblyCommit("a7f3f38")]
-[assembly: AssemblySemanticVersion("1.0.6-a7f3f38")]
-[assembly: AssemblyDescription("SemanticVersion=1.0.6-a7f3f38, Revision=1.0.6.369862121")]
+[assembly: AssemblyCommit("c758f94")]
+[assembly: AssemblySemanticVersion("1.0.6-c758f94")]
+[assembly: AssemblyDescription("SemanticVersion=1.0.6-c758f94, Revision=1.0.6.647369339")]
 [assembly: AssemblyProduct("bam.remote")]
